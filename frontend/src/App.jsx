dbeac060840324d38f354bc2eb8cab0c954f2009
SumitSahu444 <sumitkumarsahu141@gmail.com>
2024-12-30 19:07:46 +0530

    add patient pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login/Login'
 import {Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Patient from './pages/Patient/Patient'
 const App = () => {
  
   const url = "http://localhost:5000";
@@ -13,6 +14,7 @@ const App = () => {
           <Routes>
           <Route path="/" element={<Login url={url}/>}/>
            <Route path="/dashboard" element={<Dashboard url={url}/>}/>
+           <Route path="/patient" element={<Patient />}/>
         
         </Routes>
     </div>
