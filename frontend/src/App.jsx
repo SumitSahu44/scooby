76cdace2b3b47b35967c7de7a9f64d644522401b
SumitSahu444 <sumitkumarsahu141@gmail.com>
2024-12-31 19:28:16 +0530

    fix refresh problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Login from './pages/Login/Login'
-import {Routes, Route} from 'react-router-dom'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Patient from './pages/Patient/Patient'
-import Predict from './pages/Predict/Predict'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Patient from './pages/Patient/Patient';
+import Predict from './pages/Predict/Predict';
+
 const App = () => {
- 
- 
   return (
     <div>
-          <Navbar/>
-
-          <Routes>
-          <Route path="/" element={<Login/>}/>
-           <Route path="/dashboard" element={<Dashboard/>}/>
-           <Route path="/patient" element={<Patient />}/>
-           <Route path="/predict" element={<Predict  />}/>
-        </Routes>
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/patient" element={<Patient />} />
+        <Route path="/predict" element={<Predict />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
